25d2307607be6001cf4eb07ac1a9f289a061c6c1
Mohammadreza Nedaei <162311018+mohammadrezaNedaei@users.noreply.github.com>
2024-10-02 22:53:47 +0330

    run time log

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const port = 3000;
+  const date = new Date();
   const app = await NestFactory.create(AppModule);
   await app.listen(port);
   console.log(`listning on https://localhost:${port}`);
+  console.log(date.toLocaleString());
 }
 bootstrap();
