95873dd419be7ac8037e382cceca68b19c8867f2
Mohammadreza Nedaei <162311018+mohammadrezaNedaei@users.noreply.github.com>
2024-10-03 11:45:34 +0330

    change the location of db connection

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://mohammadrezanedaeei:mmd1234@myfirstdb.gsevi.mongodb.net/?retryWrites=true&w=majority&appName=myFirstDb',
+    ),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
 })
